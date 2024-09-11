1bb85f74b34a5b68228baaef7e4d2b5544350380
Jacinto López Hernández <keldaur1@gmail.com>
2024-09-11 16:28:19 +0200

    added selection grey out

--- src/components/MapItem.jsx
+++ src/components/MapItem.jsx
@@ -1,27 +1,33 @@
 import { Checkbox, Grid } from "@mui/material"
-import React, {useContext} from 'react';
+import React, {useState} from 'react';
 import { StyledTitle } from "./StyledTitle";
-import { Navigate } from "react-router-dom";
-import MissionContext from "../MissionContext";
+
+const style = {width: '100%', height: 'auto' };
+const greyedStyle = {width: '100%', height: 'auto', filter:'grayscale(100%)' }
 
 
-export const MapItem = (map, index) => {
+export const MapItem = ({map, index, handleSelectMap}) => {
+    const [checked, setChecked] = useState(false)
+
+    const handleChange = (event) => {
+        setChecked(event.target.checked);
+    }
     
-    const {selectedMission, setSelectedMap, selectedTournamentType} = useContext(MissionContext);
-    const handleSelectMap = (map) => {
-        setSelectedMap(map);
-        Navigate('/mission-details');
-      };
-    
-    return  <Grid item key={index} xs={6}>
-                <StyledTitle variant="h7" onClick={() =>  handleSelectMap(map)}>Table {++index}</StyledTitle>
-                <Checkbox  style={{marginLeft:'50%'}}/>
-                <img
-                    src={map.image}
-                    alt={map.name}
-                    style={{ width: '100%', height: 'auto' }} 
-                    onClick={() =>  handleSelectMap(map)}
-                />
-            </Grid>
-          
+    return  (
+        <Grid item key={index} xs={6}>
+            <StyledTitle variant="h7" onClick={() =>  handleSelectMap(map)}>Table {++index}</StyledTitle>
+            <Checkbox  
+                style={{marginLeft:'50%'}} 
+                onChange={handleChange}
+                checked={checked}
+                inputProps={{ 'aria-label': 'controlled' }}    
+            />
+            <img
+                src={map.image}
+                alt={map.name}
+                style={checked ? greyedStyle : style} 
+                onClick={() =>  handleSelectMap(map)}
+            />
+        </Grid>
+    )  
 }
