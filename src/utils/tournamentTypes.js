067d4d38b8c746800e0b14b3fa112b0834fd5ab2
Jacinto López Hernández <keldaur1@gmail.com>
2024-10-22 09:03:13 +0200

    remove assemble 2025

--- src/utils/tournamentTypes.js
+++ src/utils/tournamentTypes.js
@@ -1,3 +1,3 @@
-import { assemble2025, talavera2024, teams, warmaster } from "../constants/constants";
+import { talavera2024, teams, warmaster } from "../constants/constants";
 
-export const tournamentTypes = [teams, warmaster, assemble2025, talavera2024]
+export const tournamentTypes = [teams, warmaster, talavera2024]
