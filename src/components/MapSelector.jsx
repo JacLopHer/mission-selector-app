62524fe9798a394cdfa3df10fbe15bcc081ae548
JacLopHer <85135734+JacLopHer@users.noreply.github.com>
2025-03-27 13:49:16 +0000

    add top bar (#6)

--- src/components/MapSelector.jsx
+++ src/components/MapSelector.jsx
@@ -2,10 +2,11 @@ import React, { useContext, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import MissionContext from '../MissionContext';
 import { maps } from '../utils/maps';
-import { Button, Grid } from '@mui/material';
-import { StyledContainer, StyledTitle } from './StyledTitle';
+import { Button, Grid, AppBar, Toolbar, IconButton, Typography } from '@mui/material';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import ShuffleIcon from '@mui/icons-material/Shuffle'; // Icono para randomizar
+import { StyledContainer } from './StyledTitle';
 import MapItem from './MapItem';
-import { campingNauta, warmaster } from '../constants/constants';
 import classes from '../styles/MissionSelector.module.scss';
 
 const MapSelector = () => {
@@ -13,15 +14,12 @@ const MapSelector = () => {
   const { selectedMission, setSelectedMap, selectedTournamentType } = useContext(MissionContext);
   const possibleMaps = maps(selectedMission, selectedTournamentType.name);
   
-
   useEffect(() => {
     if(selectedTournamentType.singles){
-      setSelectedMap(possibleMaps[0])
+      setSelectedMap(possibleMaps[0]);
       navigate('/mission-details'); 
     }
-  },[])
-  
-  
+  }, [selectedTournamentType, possibleMaps, navigate, setSelectedMap]);
 
   const handleSelectMap = (map) => {
     setSelectedMap(map);
@@ -40,23 +38,30 @@ const MapSelector = () => {
 
   return (
     <div>
+      {/* Barra Superior */}
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton edge="start" color="inherit" onClick={handleGoBack}>
+            <ArrowBackIcon />
+          </IconButton>
+          <Typography variant="h6" sx={{ flexGrow: 1, textAlign: 'center' }}>
+            Select Map
+          </Typography>
+          {/* Botón para Randomizar con ícono */}
+          <IconButton color="inherit" onClick={handleRandomSelect}>
+            <ShuffleIcon />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+
+      {/* Contenido */}
       <StyledContainer>
-        <StyledTitle variant="h4">Select Map</StyledTitle>
+        <Grid container spacing={0}>
+          {possibleMaps.map((map, index) => (
+            <MapItem key={index} map={map} index={index} handleSelectMap={handleSelectMap} />
+          ))}
+        </Grid>
       </StyledContainer>
-      <Button onClick={handleRandomSelect} fullWidth  className={classes.randomButton}>
-        Select Random Map
-      </Button>
-      <Grid container spacing={0}>
-        {possibleMaps.map((map, index) => (
-          <MapItem key={index} map={map} index={index} handleSelectMap={handleSelectMap} />
-        ))}
-      </Grid>
-      <Button 
-        onClick={handleGoBack} 
-        fullWidth 
-        className={classes.backButton}>
-        Go Back
-      </Button>
     </div>
   );
 };
