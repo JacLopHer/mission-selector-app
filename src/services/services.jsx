555fe04b9bd047521d89e575819fd614e8cbee7a
Jacinto <jaclopher@outlook.com>
2025-06-05 13:46:58 +0100

    maspruebasanticorss

--- src/services/services.jsx
+++ src/services/services.jsx
@@ -1,17 +1,13 @@
-const BASE_URL = 'https://glickorankingapplication.onrender.com/api/public/players';
-
-
-  
-  export const fetchPlayers = async () => {
-    try {
-      const response = await fetch(BASE_URL);
-      if (!response.ok) {
-        throw new Error('Error fetching players');
-      }
-      const data = await response.json();
-      return data;
-    } catch (error) {
-      console.error(error);
-      return [];
+export const fetchPlayers = async () => {
+  try {
+    const response = await fetch('/api/public/players'); 
+    if (!response.ok) {
+      throw new Error('Error fetching players');
     }
-  };
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error(error);
+    return [];
+  }
+};
